f6bf06b536f360ef1f275e1e4a5f7baf6328e766
JackDS <manojit.dhar.jack@gmail.com>
2025-04-02 20:07:34 +0530

    added List tile

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -6,24 +6,26 @@ class Screen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Finish Flutter")),
+      appBar: AppBar(
+        title: Text("Finish Flutter"),
+        leading: Icon(Icons.menu, color: Colors.white),
+      ),
       body: Stack(
         children: [
-          Image.asset(
-            'assets/images/bg.jpg',
-            fit: BoxFit.cover,
-            height: double.infinity,
-          ),
-          Center(
-            child: Text(
-              "data",
-              style: TextStyle(
-                fontSize: 40,
-                fontFamily: 'BlackOpsOne',
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
+          // Image.asset(
+          //   'assets/images/bg.jpg',
+          //   fit: BoxFit.cover,
+          //   height: double.infinity,
+          // ),
+          Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.star),
+                tileColor: Colors.red,
+                title: Text("data"),
+                trailing: Text('trailing'),
+              )
+            ],
           ),
         ],
       ),
