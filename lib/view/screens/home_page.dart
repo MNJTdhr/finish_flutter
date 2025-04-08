adca32cb95af3047f32835fa5d9d83e7f399d146
JackDS <manojit.dhar.jack@gmail.com>
2025-04-08 23:29:47 +0530

    updated home page

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -1,56 +1,13 @@
 import 'package:flutter/material.dart';
 
 //view/screens/home_page.dart
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() => setState(() => _counter++);
-  void _decrementCounter() => setState(() => _counter--);
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () => _incrementCounter(),
-                child: Icon(Icons.add_box_outlined),
-              ),
-              ElevatedButton(
-                onPressed: () => _decrementCounter(),
-                child: Icon(Icons.remove_circle_outline_rounded),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Center(child: Text("data: $_counter")),
-        ],
-      ),
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            child: Icon(Icons.add),
-          ),
-          SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            child: Icon(Icons.remove),
-          ),
-        ],
-      ),
+    return Center(
+      child: Text("Home page"),
     );
   }
 }
