a717f2f3ac4a0b51dc4f40624a133f29c6970bfd
JackDS <manojit.dhar.jack@gmail.com>
2025-04-12 00:06:26 +0530

    added checkbox

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController textController = TextEditingController();
+  bool? isCheck = false;  //because can be null later in the code
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +23,18 @@ class _HomePageState extends State<HomePage> {
             decoration: InputDecoration(border: OutlineInputBorder()),
             onEditingComplete: () => setState(() {}),
           ),
+          SizedBox(height: 10),
           Text(textController.text),
+          SizedBox(height: 10),
+          Checkbox(
+            value: isCheck,
+            onChanged: (bool? value) {  //because can be null
+              setState(() {
+                isCheck = value;
+              });
+            },
+          ),
+          SizedBox(height: 10),
         ],
       ),
     );
