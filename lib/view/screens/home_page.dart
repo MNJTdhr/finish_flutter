c1af6f16a0a35a38ba2a0c918aebb76e69383659
JackDS <manojit.dhar.jack@gmail.com>
2025-04-11 23:37:04 +0530

    enter to display text

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 //view/screens/home_page.dart
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +17,12 @@ class HomePage extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: Column(
         children: [
-          TextField(decoration: InputDecoration(border: OutlineInputBorder())),
+          TextField(
+            controller: textController,
+            decoration: InputDecoration(border: OutlineInputBorder()),
+            onEditingComplete: () => setState(() {}),
+          ),
+          Text(textController.text),
         ],
       ),
     );
