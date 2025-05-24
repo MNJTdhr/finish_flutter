e61ef521040d9e24cc0f7f371e9da35dbd205959
JackDS <manojit.dhar.jack@gmail.com>
2025-05-25 00:20:01 +0530

    next user input section

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -18,71 +18,106 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: textController,
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            onEditingComplete: () => setState(() {}),
-          ),
-          SizedBox(height: 10),
-          Text(textController.text),
-          SizedBox(height: 10),
-          Checkbox(
-            value: isCheck,
-            onChanged: (bool? value) {
-              //because can be null
-              setState(() {
-                isCheck = value;
-              });
-            },
-          ),
-          SizedBox(height: 10),
-          CheckboxListTile(
-            tristate: true,
-            title: Text("data"),
-            value: isTriCheck,
-            onChanged: (bool? value) {
-              //because can be null
-              setState(() {
-                isTriCheck = value;
-              });
-            },
-          ),
-          SizedBox(height: 10),
-          Switch(
-            value: isSwitch,
-            onChanged:
-                (value) => setState(() {
-                  isSwitch = value;
-                }),
-          ),
-          SizedBox(height: 10),
-          SwitchListTile(
-            title: Text("data"),
-            value: isTileSwitch,
-            onChanged:
-                (value) => setState(() {
-                  isTileSwitch = value;
-                }),
-          ),
-          SizedBox(height: 10),
-          Slider(
-            value: sliderVal,
-            max: 100,
-            min: -100,
-            divisions: 10,
-            onChanged: (value) {
-              setState(() {
-                sliderVal = value;
-              debugPrint('$value');
-              });
-            },
-          ),
-          SizedBox(height: 10),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: textController,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () => setState(() {}),
+            ),
+            SizedBox(height: 10),
+            Text(textController.text),
+            SizedBox(height: 10),
+            Checkbox(
+              value: isCheck,
+              onChanged: (bool? value) {
+                //because can be null
+                setState(() {
+                  isCheck = value;
+                });
+              },
+            ),
+            SizedBox(height: 10),
+            CheckboxListTile(
+              tristate: true,
+              title: Text("data"),
+              value: isTriCheck,
+              onChanged: (bool? value) {
+                //because can be null
+                setState(() {
+                  isTriCheck = value;
+                });
+              },
+            ),
+            SizedBox(height: 10),
+            Switch(
+              value: isSwitch,
+              onChanged:
+                  (value) => setState(() {
+                    isSwitch = value;
+                  }),
+            ),
+            SizedBox(height: 10),
+            SwitchListTile(
+              title: Text("data"),
+              value: isTileSwitch,
+              onChanged:
+                  (value) => setState(() {
+                    isTileSwitch = value;
+                  }),
+            ),
+            SizedBox(height: 10),
+            Slider(
+              value: sliderVal,
+              max: 100,
+              min: -100,
+              divisions: 10,
+              onChanged: (value) {
+                setState(() {
+                  sliderVal = value;
+                  debugPrint('$value');
+                });
+              },
+            ),
+            SizedBox(height: 10),
+            Checkbox(
+              value: isCheck,
+              onChanged: (bool? value) {
+                //because can be null
+                setState(() {
+                  isCheck = value;
+                });
+              },
+            ),
+            SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                debugPrint("pressed");
+              },
+              child: Image.asset('assets/images/bg.jpg'),
+            ),
+            SizedBox(height: 10),
+            InkWell(
+              onTap: () {
+                debugPrint("pressed");
+              },
+              splashColor: Colors.teal,
+              child: Container(
+                color: Colors.white12,
+                width: double.infinity,
+                height: 200,
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {}, child: Text("Click me")),
+            TextButton(onPressed: () {}, child: Text("Click me")),
+            OutlinedButton(onPressed: () {}, child: Text("Click me")),
+            FilledButton(onPressed: () {}, child: Text("Click me")),
+          ],
+        ),
       ),
     );
   }
